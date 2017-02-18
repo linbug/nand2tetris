9e9281752c27eaf09d03fb0dc0cac2dbf14fb58d
Lin Taylor <taylorxlin@gmail.com>
2017-02-18 20:50:14 +0000

    'Add jump dict'

--- projects/06/code_module.py
+++ projects/06/code_module.py
@@ -1,5 +1,5 @@
     dest_dict = {
-        '': '000',
+        'null': '000',
         'M': '001',
         'D': '010',
         'MD': '011',
@@ -9,7 +9,22 @@
         'AMD': '111',
     }
 
+    jump_dict = {
+        'null': '000',
+        'JQT': '001',
+        'JEQ': '010',
+        'JGE': '011',
+        'JLT': '100',
+        'JNE': '101',
+        'JLE': '110',
+        'JMP': '111',
+    }
+
 def dest(command):
     return dest_dict[command]
 
 def jump(command):
+    return jump_dict[command]
+
+def comp(command):
+    return comp_dict[command]
