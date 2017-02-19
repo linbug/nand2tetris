bbf1ac8df54beeb0f7188a561c5b8d422adf3560
Lin Taylor <taylorxlin@gmail.com>
2017-02-19 13:25:32 +0000

    Add main module (to complete)

---
+++ projects/06/main.py
@@ -0,0 +1,15 @@
+import sys
+
+from code_module import dest, jump, comp
+from parser import Parser
+
+
+def main(path_to_file):
+    # read in line
+    # break down into separate command
+    #  
+
+
+
+if __name__ == '__main__':
+    main(sys.argv[0])
