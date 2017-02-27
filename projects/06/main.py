7fe52dfd2237fddce2bc0cb10bea2a5e5776b24f
Ramana Kumar <ramana@member.fsf.org>
2017-02-27 20:00:32 +1100

    Get the main driver working
    Changes: calculate the destination file name, write nothing for L
    commands, and write the newline for each line

--- projects/06/main.py
+++ projects/06/main.py
@@ -7,7 +7,9 @@ def main(file_name):
     with open(file_name) as f:
         file_contents = f.read()
     parser = Parser(input=file_contents)
-    with open(destination_file) as d:
+    basename = file_name[:file_name.find(".asm")]
+    dest_name = basename+".hack"
+    with open(basename+".hack",'w') as d:
         while parser.hasMoreCommands():
             parser.advance()
             current_command_type = parser.commandType()
@@ -15,11 +17,7 @@ def main(file_name):
                 d.write('0' + parser.symbol())
             elif current_command_type == 'C_command':
                 d.write('111' + parser.comp() + parser.dest() + parser.jump())
-            elif current_command_type == 'L_command':
-
-            
-
-
+            d.write("\n")
 
 if __name__ == '__main__':
     main(sys.argv[1])
